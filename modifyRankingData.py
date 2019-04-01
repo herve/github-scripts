0482c7d338bc67810ec0bd2a33b6be32fd22321e
herve <h.le.turdu@gmail.com>
2019-04-01 22:50:01 +0200

    Create modifyRankingData.py
    importing workbook, stats, regex, sys modules

---
+++ modifyRankingData.py
@@ -0,0 +1,9 @@
+# import Workbook module
+from openpyxl import Workbook
+# import regex module
+import re
+# import stats module
+import pandas as pd
+import numpy as np
+# import to debug to stop script
+import sys 
